3de143f4065b2f8c6d71722ef849c1cfbdc4da46
林小龙 <xiaolong.lin@ele.me>
2017-11-11 19:39:42 +0800

    refactor: code refine

--- src/api/customer/index.js
+++ src/api/customer/index.js
@@ -12,9 +12,9 @@ const create = params => oib.post(`${base}`, params).then(res => res.data);
 
 const save = (id, params) => oib.put(`${base}/${id}`, params).then(res => res.data);
 
-const getSliders = () => oib.get('customer-sliders');
+const getSliders = () => oib.get('customer-sliders').then(res => res.data);
 
-const saveSliders = data => oib.post('customer-sliders', data);
+const saveSliders = data => oib.post('customer-sliders', data).then(res => res.data);
 
 export default {
   get,
--- src/pages/services/sliders/sliders.vue
+++ src/pages/services/sliders/sliders.vue
@@ -3,17 +3,18 @@
     title="修改轮播图"
     :visible.sync="dialogVisible">
     <el-form label-width="150px" label-position="top">
-      <el-form-item :label="item.label" v-for="(item, index) of sliders" :key="index">
+      <el-form-item :label="`第${index + 1}张`" v-for="(item, index) of sliders" :key="index">
         <el-col :span="24">
           <op-upload-img
             v-model="item.value"
             ref="bannerImgUpload"></op-upload-img>
-          <!--<img class="preview-img" :src="item.value">-->
+          <el-button type="success" @click="addPicture()" v-if="index == sliders.length - 1">添加图片</el-button>
+          <el-button type="danger" @click="removePicture(index)"  v-if="index !== 0">移除图片</el-button>
         </el-col>
       </el-form-item>
     </el-form>
     <span slot="footer" class="dialog-footer">
-      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button @click="cancel">取 消</el-button>
       <el-button type="primary" @click="confirm">确 定</el-button>
     </span>
   </el-dialog>
@@ -29,25 +30,38 @@
     data() {
       return {
         dialogVisible: false,
-        sliders: ['', '', '', '', '', '']
+        sliders: [],
+        preSliders: []
       };
     },
     async created() {
-      this.sliders = ((await customerApi.getSliders()).data.sliders || this.sliders)
-        .map((item, index) => ({
-          label: `第${index + 1}张`,
-          value: item
-        }));
+      this.sliders = ((await customerApi.getSliders()).sliders || [''])
+        .map(item => ({ value: item }));
+      this.preSliders = [...this.sliders];
     },
     methods: {
       open() {
         this.dialogVisible = true;
+        this.sliders = [...this.preSliders];
       },
       confirm() {
         customerApi.saveSliders({
-          sliders: this.sliders.map(item => item.value)
-        }).then(() => {
+          sliders: this.sliders.filter(item => item.value).map(item => item.value)
+        }).then(({ sliders }) => {
           this.dialogVisible = false;
+          this.sliders = sliders.map(item => ({ value: item }));
+          this.preSliders = [...this.sliders];
+        });
+      },
+      cancel() {
+        this.dialogVisible = false;
+      },
+      removePicture(index) {
+        this.sliders.splice(index, 1);
+      },
+      addPicture() {
+        this.sliders.push({
+          value: ''
         });
       }
     }
