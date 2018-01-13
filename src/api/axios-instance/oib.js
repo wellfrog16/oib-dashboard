145c09b2920736a897fa83f3aa8ee376a85b9e94
林小龙 <xiaolong.lin@ele.me>
2018-01-14 00:24:27 +0800

    fix

--- src/api/axios-instance/oib.js
+++ src/api/axios-instance/oib.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { Loading, Notification } from 'element-ui';
 
-const baseURL = 'http://localhost:9999/oib-api';
+// const baseURL = 'http://localhost:9999/oib-api';
+const baseURL = 'http://oib-cms.test.tron-m.com/oib-api';
 
 const instance = axios.create({
   // baseURL: 'http://www.tron-m.com/oib-api',
@@ -34,7 +35,8 @@ instance.interceptors.response.use((response) => {
     });
     if (data.code === 401) {
       // 需要登录
-      window.location.href = `${window.location.origin}/#/login`;
+      const { origin, pathname } = window.location;
+      window.location.href = `${origin}${pathname}/#/login`;
     }
     return Promise.reject(data.msg);
   }
